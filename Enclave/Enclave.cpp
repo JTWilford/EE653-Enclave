3c021aff6a3639f13aab3c767104bb698de24471
Justin Wilford <jwilford@usc.edu>
2022-03-10 22:22:08 -0800

    Switched back to row major

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -23,6 +23,7 @@ void read_rand(float *r, int totalSize) {
     }
 }
 
+// Row Major
 void matrix_mult(float *a, int a_rows, int a_cols, float *b, int b_rows, int b_cols, float *out) {
     printf("Dims: a=%dx%d, b=%dx%d, out=%dx%d\n", a_rows, a_cols, b_rows, b_cols, a_rows, b_cols);
     for (int i = 0; i < a_rows; i++) {
@@ -30,7 +31,7 @@ void matrix_mult(float *a, int a_rows, int a_cols, float *b, int b_rows, int b_c
         for (int j = 0; j < b_cols; j++) {
             for (int k = 0; k < a_cols; k++) {
                 // printf("%d = %d * %d\n", i * inp_cols + j, i * w_cols + k, k * inp_cols + j);
-                out[a_cols*j + i] += a[a_cols*k + i] * b[b_cols*j + k];
+                out[a_cols*i + j] += a[a_cols*i + k] * b[b_cols*k + j];
             }
         }
     }
@@ -39,17 +40,46 @@ void matrix_mult(float *a, int a_rows, int a_cols, float *b, int b_rows, int b_c
 void matrix_add(float *a, int a_rows, int a_cols, float *b, float *out) {
     for (int i = 0; i < a_cols; i++) {
         for (int j = 0; j < a_rows; j++) {
-            out[a_cols*j + i] = a[a_cols*j + i] + b[a_cols*j + i];
+            out[a_cols*i + j] = a[a_cols*i + j] + b[a_cols*i + j];
         }
     }
 }
 void matrix_sub(float *a, int a_rows, int a_cols, float *b, float *out) {
-    for (int i = 0; i < a_cols; i++) {
+    for (int i = 0; i < a_rows; i++) {
         for (int j = 0; j < a_rows; j++) {
-            out[a_cols*j + i] = a[a_cols*j + i] - b[a_cols*j + i];
+            out[a_cols*i + j] = a[a_cols*i + j] - b[a_cols*i + j];
         }
     }
 }
+
+// Column Major
+// void matrix_mult(float *a, int a_rows, int a_cols, float *b, int b_rows, int b_cols, float *out) {
+//     printf("Dims: a=%dx%d, b=%dx%d, out=%dx%d\n", a_rows, a_cols, b_rows, b_cols, a_rows, b_cols);
+//     for (int i = 0; i < a_rows; i++) {
+//         printf("i=%d\r", i);
+//         for (int j = 0; j < b_cols; j++) {
+//             for (int k = 0; k < a_cols; k++) {
+//                 // printf("%d = %d * %d\n", i * inp_cols + j, i * w_cols + k, k * inp_cols + j);
+//                 out[a_rows*j + i] += a[a_rows*k + i] * b[b_rows*j + k];
+//             }
+//         }
+//     }
+// }
+
+// void matrix_add(float *a, int a_rows, int a_cols, float *b, float *out) {
+//     for (int i = 0; i < a_cols; i++) {
+//         for (int j = 0; j < a_rows; j++) {
+//             out[a_rows*j + i] = a[a_rows*j + i] + b[a_rows*j + i];
+//         }
+//     }
+// }
+// void matrix_sub(float *a, int a_rows, int a_cols, float *b, float *out) {
+//     for (int i = 0; i < a_cols; i++) {
+//         for (int j = 0; j < a_rows; j++) {
+//             out[a_rows*j + i] = a[a_rows*j + i] - b[a_rows*j + i];
+//         }
+//     }
+// }
 
 
 // the actual buffer of *inp is in untrusted memory
