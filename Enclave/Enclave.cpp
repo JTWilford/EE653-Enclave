33229adabe42938d2d9e9c443d81b4c6a1d7e025
Justin Wilford <jwilford@usc.edu>
2022-03-10 17:59:03 -0800

    Replaced read_rand

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -17,10 +17,16 @@ int printf(const char* fmt, ...)
     return (int)strnlen(buf, BUFSIZ - 1) + 1;
 }
 
+void read_rand(float *r, int totalSize) {
+    for (int i = 0; i < totalSize; i++) {
+        r[i] = (float) i;
+    }
+}
+
 
 // the actual buffer of *inp is in untrusted memory
 // You can read from it, but never write to it
-int ecall_compute_secrete_operation(int* inp, int size) {
+int ecall_compute_secrete_operation(int *inp, int size) {
     // decrypt inp
     // ....
 
@@ -38,25 +44,25 @@ int ecall_compute_secrete_operation(int* inp, int size) {
 }
 
 // Computes w * inp, and writes the data into buffer out
-// float* w => 2d array of weights
-// int* dimW => 2-element array defining the size of w
-// float* inp => 2d array representing input vector
-// int* dimInp => 2-element array defining the size of inp
-// float* out => Output buffer
-void ecall_nativeMatMul(float* w, int* dimW, float* inp, int* dimInp, float* out) {
+// float *w => 2d array of weights
+// int *dimW => 2-element array defining the size of w
+// float *inp => 2d array representing input vector
+// int *dimInp => 2-element array defining the size of inp
+// float *out => Output buffer
+void ecall_nativeMatMul(float *w, int *dimW, float *inp, int *dimInp, float *out) {
     printf("nativeMatMul\n");
     // Copy the W array out of untrusted memory
     int w_rows = dimW[0];
     int w_cols = dimW[1];
-    float* w_cpy = (float*) malloc(sizeof(float) * w_rows * w_cols);
+    float *w_cpy = (float*) malloc(sizeof(float) * w_rows * w_cols);
     memcpy(w_cpy, w, sizeof(float) * w_rows * w_cols);
     int inp_rows = dimInp[0];
     int inp_cols = dimInp[1];
-    float* inp_cpy = (float*) malloc(sizeof(float) * inp_rows * inp_cols);
+    float *inp_cpy = (float*) malloc(sizeof(float) * inp_rows * inp_cols);
     memcpy(inp_cpy, inp, sizeof(float) * inp_rows * inp_cols);
 
     // Perform matrix multiplication
-    float* res = (float*) malloc(sizeof(float) * w_rows * inp_cols);
+    float *res = (float*) malloc(sizeof(float) * w_rows * inp_cols);
     for (int i = 0; i < w_rows; i++) {
         for (int j = 0; j < inp_cols; j++) {
             for (int k = 0; k < w_cols; k++) {
@@ -71,17 +77,17 @@ void ecall_nativeMatMul(float* w, int* dimW, float* inp, int* dimInp, float* out
 
 // Generate an array of random floats r, then compute r * weight
 // The results of this operation are stored inside the enclave
-// float* weight => 2d array of Weights
-// int* dim => 2-element array defining the size of weight
+// float  *weight => 2d array of Weights
+// int *dim => 2-element array defining the size of weight
 // int batch => ???
-float* r = nullptr;
-float* w_pre = nullptr;
-void ecall_precompute(float* weight, int* dim, int batch) {
+float *r = nullptr;
+float *w_pre = nullptr;
+void ecall_precompute(float *weight, int *dim, int batch) {
     printf("precompute\n");
     // Copy weight out of untrusted memory
     int weight_rows = dim[0];
     int weight_cols = dim[1];
-    float* weight_cpy = (float*) malloc(sizeof(float) * weight_rows * weight_cols);
+    float *weight_cpy = (float*) malloc(sizeof(float) * weight_rows * weight_cols);
     printf("1\n");
     memcpy(weight_cpy, weight, sizeof(float) * weight_rows * weight_cols);
     // Generate random numbers in r
@@ -90,7 +96,7 @@ void ecall_precompute(float* weight, int* dim, int batch) {
     }
     r = (float*) malloc(sizeof(float) * batch * weight_rows);
     printf("2\n");
-    sgx_read_rand((unsigned char*) r, sizeof(float) * batch * weight_rows);
+    read_rand((unsigned char*) r, sizeof(float) * batch * weight_rows);
     printf("3\n");
 
     // Perform matrix multiplication
@@ -114,16 +120,16 @@ void ecall_precompute(float* weight, int* dim, int batch) {
 
 // Computes inp + r, where r is a random buffer that was populated
 // by ecall_precompute
-void ecall_addNoise(float* inp, int* dim, float* out) {
+void ecall_addNoise(float *inp, int *dim, float *out) {
     printf("addNoise\n");
     // Copy input out of untrusted memory
     int inp_rows = dim[0];
     int inp_cols = dim[1];
-    float* inp_cpy = (float*) malloc(sizeof(float) * inp_rows * inp_cols);
+    float *inp_cpy = (float*) malloc(sizeof(float) * inp_rows * inp_cols);
     memcpy(inp_cpy, inp, sizeof(float) * inp_rows * inp_cols);
 
     // Perform matrix addition
-    float* res = (float*) malloc(sizeof(float) * inp_rows * inp_cols);
+    float *res = (float*) malloc(sizeof(float) * inp_rows * inp_cols);
     for (int i = 0; i < inp_rows; i++) {
         for (int j = 0; j < inp_cols; j++) {
             res[i*inp_rows + j] = inp_cpy[i*inp_rows + j] + r[i*inp_rows + j];
@@ -135,16 +141,16 @@ void ecall_addNoise(float* inp, int* dim, float* out) {
 
 // Computes inp - (r * w). r * w has been precomputed by ecall_precompute
 
-void ecall_removeNoise(float* inp, int* dim, float* out) {
+void ecall_removeNoise(float *inp, int *dim, float *out) {
     printf("removeNoise\n");
     // Copy input out of untrusted memory
     int inp_rows = dim[0];
     int inp_cols = dim[1];
-    float* inp_cpy = (float*) malloc(sizeof(float) * inp_rows * inp_cols);
+    float *inp_cpy = (float*) malloc(sizeof(float) * inp_rows * inp_cols);
     memcpy(inp_cpy, inp, sizeof(float) * inp_rows * inp_cols);
 
     // Perform matrix substraction
-    float* res = (float*) malloc(sizeof(float) * inp_rows * inp_cols);
+    float *res = (float*) malloc(sizeof(float) * inp_rows * inp_cols);
     for (int i = 0; i < inp_rows; i++) {
         for (int j = 0; j < inp_cols; j++) {
             res[i*inp_rows + j] = inp_cpy[i*inp_rows + j] - w_pre[i*inp_rows * j];
