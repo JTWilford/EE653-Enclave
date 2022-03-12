d1bc44feec317d7bf67ad386b361743478444843
Justin Wilford <jwilford@usc.edu>
2022-03-11 20:36:49 -0800

    Added metrics to proj2.py

--- python/proj2.py
+++ python/proj2.py
@@ -1,50 +1,82 @@
 import torch
+import time
 
 from python.sgxutils import SGXUtils
 
 def main(args):
     sgxutils = SGXUtils()
 
-    # For whatever reason, Weights is Row-Major and X is Column-Major. This is stupid.
+    fast_times = []
+    slow_times = []
+    speedups = []
+    for i in range(1, 6):
+        print("============================")
+        print("\tRun #%d" % i)
+        print("============================")
+        l = torch.nn.Linear(args.in_features, args.out_features, bias=False).cuda()
+        x = torch.randn(args.batch, args.in_features).cuda()
+        w = l.weight
 
-    l = torch.nn.Linear(args.in_features, args.out_features, bias=False).cuda()
-    x = torch.randn(args.batch, args.in_features).cuda()
-    w = l.weight
-    w_t = torch.transpose(l.weight, 0, 1)
-    x_t = torch.transpose(x, 0, 1)
-
-    print("Weight (%d, %d):" % (w.size(0), w.size(1)))
-    # print(w)
-    print("X (%d, %d):" % (x.size(0), x.size(1)))
-    # print(x)
+        print("Weight (%d, %d):" % (w.size(0), w.size(1)))
+        # print(w)
+        print("X (%d, %d):" % (x.size(0), x.size(1)))
+        # print(x)
 
 
-    # given the weight; precompute w * r
-    sgxutils.precompute(w, args.batch)
+        # Time tracking for Fast Mult
+        start_time = time.time()
+        # given the weight; precompute w * r
+        sgxutils.precompute(w, args.batch)
 
-    # x_blinded = x + r
-    x_blinded = sgxutils.addNoise(x)
-    # print("x_blinded:")
-    # print(x_blinded)
+        # x_blinded = x + r
+        x_blinded = sgxutils.addNoise(x)
+        # print("x_blinded:")
+        # print(x_blinded)
 
-    # y_blinded = w * x_blinded
-    y_blinded = l(x_blinded)
-    # print("y_blinded:")
-    # print(y_blinded)
+        # y_blinded = w * x_blinded
+        y_blinded = l(x_blinded)
+        # print("y_blinded:")
+        # print(y_blinded)
 
-    # y_recovered = y_blinded - w * r
-    y_recovered = sgxutils.removeNoise(y_blinded)
-    # print("y_recovered:")
-    # print(y_recovered)
-    s = sgxutils.nativeMatMul(w, x)
-    # print("s")
-    # print(s)
+        # y_recovered = y_blinded - w * r
+        y_recovered = sgxutils.removeNoise(y_blinded)
+        # print("y_recovered:")
+        # print(y_recovered)
+        fast_time = time.time() - start_time
 
-    y_expected = l(x)
-    # print("y_expected:")
-    # print(y_expected)
+        # Time tracking for Native Mult
+        start_time = time.time()
+        s = sgxutils.nativeMatMul(w, x)
+        # print("s")
+        # print(s)
+        slow_time = time.time() - start_time
 
-    print("Total diffs:", abs(y_expected - y_recovered).sum())
+        y_expected = l(x)
+        # print("y_expected:")
+        # print(y_expected)
+
+        print("Total diffs:", abs(y_expected - y_recovered).sum())
+
+        fast_times.append(fast_time)
+        slow_times.append(slow_time)
+        speedups.append(slow_time / fast_time)
+
+    print("----------------------")
+    print("Fast Times:\t%d" % (fast_times))
+    print("Native Times:\t%d" % (slow_times))
+    print("Speedups:\t%d" % (speedups))
+    print()
+    t_sum = 0
+    for t in fast_times:
+        t_sum += t
+    fast_avg = t_sum / len(fast_times)
+    t_sum = 0
+    for t in slow_times:
+        t_sum += t
+    slow_avg = t_sum / len(slow_times)
+    print("Average Fast Time:\t%d" % fast_avg)
+    print("Average Native Time:\t%d" % slow_avg)
+    print("Average Speedup:\t%d" % (slow_avg / fast_avg))
 
     return 0
     
